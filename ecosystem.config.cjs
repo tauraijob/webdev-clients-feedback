f36be206ff457984b9fb608cb476f3d1308cc5df
Taurai Job Munodawafa <taujob1111@gmail.com>
2025-01-22 17:02:59 +0200

    port change

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'Webdev Reviews',
-        port: '2029',
+        port: '3024',
         exec_mode: 'cluster',
         instances: '1',
         script: './.output/server/index.mjs' 
